4a1ffaebbef3f1e6d6b7cf70178efbc1e0a66a10
Nidoveralis <nidoveralis@gmail.com>
2024-11-10 00:53:33 +0400

    feature: edited add hidden and animate class

--- index.js
+++ index.js
@@ -9,10 +9,11 @@ const circleDlin = 2 * Math.PI * 45;
 bar.style.strokeDasharray = circleDlin;
 bar.style.strokeDashoffset = circleDlin;
 
-
 function handleChangeVisible() {
   if (hideInput.checked) {
     circle.classList.add('hidden');
+    circle.classList.remove('animate');
+    animateInput.checked = false;
   } else {
     circle.classList.remove('hidden');
   }
@@ -21,17 +22,24 @@ function handleChangeVisible() {
 function handleChangeAnimation() {
   if (animateInput.checked) {
     circle.classList.add('animate');
+    circle.classList.remove('hidden');
+    hideInput.checked = false;
   } else {
     circle.classList.remove('animate');
   }
 };
 
 function handleChangeInput(e) {
-  const percent = e.target.value;
-  if (percent <= 100) {
-    bar.style.strokeDashoffset = circleDlin - percent / 100 * circleDlin;
-  } else {
+  let percent = e.target.value.replace(/[^0-9]/g, '');
+
+  percent = parseInt(percent, 10) || 0;
+
+  if (percent > 100) {
+    percent = 100;
   }
+
+  e.target.value = percent;
+  bar.style.strokeDashoffset = circleDlin - (percent / 100) * circleDlin;
 };
 
 hideInput.addEventListener('change', handleChangeVisible);
